dbb167795718e2e71da9d64bafef5d04d5133d49
gOK-m <71651713+gOK-m@users.noreply.github.com>
2021-07-13 19:36:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.position(600, 200);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+
+}
